3efec5a577e10b98b0b88dad546e9988227330e8
Brandon Karunakaran <brandon@appitierre.com>
2019-06-14 12:19:58 +0100

    Added in react-dom-router route for rendering login

---
+++ src/modules/authentication/containers/loginContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+
+export default class LoginContainer extends Component {
+    
+    
+    render() {
+        return (
+            <Card>
+                Hey
+            </Card>
+        );
+    }
+}
