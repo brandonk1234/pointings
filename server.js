721b3d456e8e1bd1a20e2f88c04e127791d3fca9
Joe <joe.langley@appitierre.com>
2019-06-21 10:49:59 +0100

    Implemented passport.js and basic authentication/routes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+// ------ MONGOOSE GLOBAL PROMISE ------:
+mongoose.promise = global.Promise;
 
 // ------ ROUTES ------:
 app.get('/', (req, res) => {
@@ -9,16 +15,25 @@ app.get('/', (req, res) => {
 });
 
 // ----- INIT MIDDLEWARE -----:
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'pointings', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 // ----- CONNECT TO DB -----:
 const mongoDB = 'mongodb://localhost/pointings';
 mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.set('debug', true);
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// ----- MODELS AND ROUTES -----:
+require('./src/app/models/Users');
+require('./src/app/config/passport');
+app.use(require('./src/app/routes'));
 
 // ----- RUN SERVER -----:
 const PORT = process.env.PORT || 5001;
